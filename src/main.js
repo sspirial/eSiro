feada391302aa2e02631e15fc07c13540a8b15c0
Munubi Emmanuel <sspirial@gmail.com>
2025-03-29 17:54:52 +0300

    globalizing observables

--- src/main.js
+++ src/main.js
@@ -4,16 +4,39 @@ import { db } from "./db";
 globalThis.observables = {
   names: liveQuery(() => db.names.toArray()),
 };
+globalThis.db = db
 
-// Subscribe
-const subscription = observables.names.subscribe({
-  next: (result) => console.log("Got result:", JSON.stringify(result)),
-  error: (error) => console.error(error),
-});
+console.log(globalThis)
 
-db.names.add({
-  id: 1, name: 'malgljoisdj'
-})
+class EsiroNetwork extends HTMLElement {
 
-// Unsubscribe
-subscription.unsubscribe();
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        ul { list-style: none; padding: 0; }
+      </style>
+      <h1>Names:</h1><ul id="names-list"></ul>`;
+  }
+  
+  connectedCallback() {
+    this.subscription = observables.names.subscribe({
+      next: (result) => this.renderNames(result),
+      error: (error) => console.error(error),
+    });
+  }
+  
+  disconnectedCallback() {
+    this.subscription.unsubscribe();
+  }
+  
+  renderNames(names) {
+    const namesList = this.shadowRoot.getElementById('names-list');
+    if (namesList) {
+      namesList.innerHTML = names.map(name => `<li>${name.name}</li>`).join('');
+    }
+  }
+}
+
+customElements.define('esiro-network', EsiroNetwork)
